eec5a4ad054eb62ee73937522d3ce92a30090280
Matt <matthewmorgante@gmail.com>
2015-04-07 20:54:34 +0700

    deleted unneccessary files and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   end 
   root to: 'procedures#index'
 
-  get 'procedures/import'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
